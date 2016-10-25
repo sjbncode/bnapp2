2274f167cfe682b133d516eb659e137e35607b0c
sijian.gu <sijian.gu@genewiz.com>
2016-10-25 17:08:26 +0800

    xx

---
+++ app/modules/bn-chart/bn-chart.m.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import{SharedModule}from '../share/share.m';
+
+@NgModule({
+  imports:      [ SharedModule ],
+  declarations: [  ],
+  providers:    [  ]
+})
+export class BnChartModule { 
+
+}
